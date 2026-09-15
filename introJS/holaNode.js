1bc330af79682b1a3155fb7456fb63674019b4c0
Alexita <125055470@upq.edu.mx>
2026-09-14 20:28:03 -0600

    Practica 1, introducción a js

---
+++ introJS/holaNode.js
@@ -0,0 +1,12 @@
+console.log("--- Holi mundo node---");
+
+let edad1=20;
+let edad2=7;
+
+console.log("Edad promedio: ");
+console.log((edad1+edad2)/2);
+
+console.log("---Medir procesos---");
+console.time("miProceso")
+    for(i=0;i<1000000;i++){}
+console.timeEnd("miProceso")
---
+++ introJS/logica.js
@@ -0,0 +1,3 @@
+    let nombre = prompt("¿Cuál es tu nombre?: ");
+        console.log("Hola " + nombre + "!!");
+        document.write("<h1>Holi " + nombre + "!!</h1>");
